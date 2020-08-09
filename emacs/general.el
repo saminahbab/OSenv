57fc76588da7d472e2378b112eeb39fa2baf38ce
saminahbab <saminahbab0@gmail.com>
2020-08-09 10:19:52 +0100

    org capture include

--- emacs/general.el
+++ emacs/general.el
@@ -1,4 +1,4 @@
-(setq-default custom-file "~/.gnu-emacs-custom")
+;; (setq-default custom-file "~/.gnu-emacs-custom")
 
 ;; Don't use lock files (which also clutter up my view of a directory.
 (setq create-lockfiles nil)
@@ -7,7 +7,9 @@
 ;; Use Menlo as default font
 (set-frame-font "Menlo 10" nil t)
 
+;; remap keys
 
+(define-key global-map (kbd "M-k") 'kill-this-buffer)
 
 ;;save directory is away from project
 (setq backup-directory-alist '(("." . "~/.saves")))
@@ -20,12 +22,13 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; theming ?
-(use-package spaceline
+(use-package
+  spaceline
   :after (spaceline-emacs-theme)
-  :ensure t )
+  :ensure t)
 
 
-;; maximise real estate and get rid of splash
+;; maximise real estate and get rid of splasH
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -39,7 +42,8 @@
 
 ;; Remove useless whitespace before saving a file
 (add-hook 'before-save-hook 'whitespace-cleanup)
-(add-hook 'before-save-hook (lambda() (delete-trailing-whitespace)))
+(add-hook 'before-save-hook (lambda()
+			      (delete-trailing-whitespace)))
 
 
 ;; Smart-mode-line makes the mode-line better to read
@@ -50,120 +54,116 @@
 
 (setq-default inhibit-splash-screen t)
 
-(use-package savehist
-  :config
-    (setq history-length 10000))
+(use-package
+  savehist
+  :config (setq history-length 10000))
 (savehist-mode)
 
-(use-package company
+(use-package
+  company
   :ensure t
-  :init
-  (add-hook 'after-init-hook 'global-company-mode)
-  :config
-  (setq company-idle-delay 0)
+  :init (add-hook 'after-init-hook 'global-company-mode)
+  :config (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 1))
 
 
-(use-package counsel
+(use-package
+  counsel
   :config
-    ;; Use virtual buffers, this adds bookmarks and recentf to the
-    ;; switch-buffer function:
-    (setq ivy-use-virtual-buffers t)
-    ;; Candidate count format for ivy read. Show index and count.
-    (setq ivy-count-format "(%d/%d) ")
-    ;; I use big windows, so plenty of room for ivy mini buffer
-    (setq max-mini-window-height 0.5)
-    (setq ivy-height 20)
+  ;; Use virtual buffers, this adds bookmarks and recentf to the
+  ;; switch-buffer function:
+  (setq ivy-use-virtual-buffers t)
+  ;; Candidate count format for ivy read. Show index and count.
+  (setq ivy-count-format "(%d/%d) ")
+  ;; I use big windows, so plenty of room for ivy mini buffer
+  (setq max-mini-window-height 0.5)
+  (setq ivy-height 20)
   :bind
-    ;; Some standard keybindings, matching helm ones I used to have.
-    (("M-s o" . 'swiper)
-     ("M-x" . 'counsel-M-x)
-     ("C-x C-f" . 'counsel-find-file)
-     ;; These keybindings recommended by counsel docs.
-     ("<f1> f" . 'counsel-describe-function)
-     ("<f1> v" . 'counsel-describe-variable)
-     ("<f1> l" . 'counsel-find-library)
-     ("<f2> i" . 'counsel-info-lookup-symbol)
-     ("<f2> u" . 'counsel-unicode-char)
-     ;; use counsel to lookup bookmarks
-     ("C-x r l" . 'counsel-bookmark)
-     ;; Old keybindings I had from before switching to helm/ivy
-     ("<f11>" . nil)
-     ("<f11> s" . 'swiper)
-     ("<f11> g l" . 'counsel-git-log)
-     ("<f11> g b" . 'counsel-git-checkout)
-     ("<f11> g a" . 'counsel-ag)
-     ("C-c o p f" . 'counsel-projectile-find-file)
-     ("C-c o f f" . 'counsel-find-file)
-     ("C-c r" . 'ivy-resume)
-     ("<f11> u" . 'counsel-unicode-char)))
+  ;; Some standard keybindings, matching helm ones I used to have.
+  (("M-s o" . 'swiper)
+   ("M-x" . 'counsel-M-x)
+   ("C-x C-f" . 'counsel-find-file)
+   ;; These keybindings recommended by counsel docs.
+   ("<f1> f" . 'counsel-describe-function)
+   ("<f1> v" . 'counsel-describe-variable)
+   ("<f1> l" . 'counsel-find-library)
+   ("<f2> i" . 'counsel-info-lookup-symbol)
+   ("<f2> u" . 'counsel-unicode-char)
+   ;; use counsel to lookup bookmarks
+   ("C-x r l" . 'counsel-bookmark)
+   ;; Old keybindings I had from before switching to helm/ivy
+   ("<f11>" . nil)
+   ("<f11> s" . 'swiper)
+   ("<f11> g l" . 'counsel-git-log)
+   ("<f11> g b" . 'counsel-git-checkout)
+   ("<f11> g a" . 'counsel-ag)
+   ("C-c z p f" . 'counsel-projectile-find-file)
+   ("C-c z f f" . 'counsel-find-file)
+   ("C-c r" . 'ivy-resume)
+   ("<f11> u" . 'counsel-unicode-char)))
 
-(use-package projectile
+(use-package
+  projectile
   :ensure t
-  :config
-  (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
-    (projectile-mode +1))
+  :config (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+  (projectile-mode +1))
 
-(use-package counsel-projectile
+(use-package
+  counsel-projectile
   :bind
-     ;; Enable ripgrep with counsel.
+  ;; Enable ripgrep with counsel.
   (("C-c f" . #'counsel-projectile-rg)))
-(use-package smex)
+(use-package
+  smex)
 
 (ivy-mode 1)
-(use-package ivy-rich
-  :init
-  (setq ivy-rich-switch-buffer-name-max-length 100)
+(use-package
+  ivy-rich
+  :init (setq ivy-rich-switch-buffer-name-max-length 100)
   (ivy-rich-mode))
-(use-package smartparens
-  :init
-(sp-use-smartparens-bindings))
+(use-package
+  smartparens
+  :init (sp-use-smartparens-bindings))
 (add-hook 'go-mode-hook #'smartparens-mode)
 (add-hook 'python-mode-hook #'smartparens-mode)
 
-(use-package flycheck :ensure t
-  :bind
-  (
-   ("C-c l p" . 'flycheck-previous-error)
-   ("C-c l n" . 'flycheck-next-error)
-   ))
+(use-package
+  flycheck
+  :ensure t
+  :bind (("C-c l p" . 'flycheck-previous-error)
+	 ("C-c l n" . 'flycheck-next-error)))
 
 ;; LSP
 
-(use-package lsp-mode
+(use-package
+  lsp-mode
   :ensure t
-  :config
-  (add-hook 'python-mode-hook #'lsp)
+  :config (add-hook 'python-mode-hook #'lsp)
   (add-hook 'go-mode-hook #'lsp)
-  (add-hook 'rust-mode-hook #'lsp)
-  )
+  (add-hook 'rust-mode-hook #'lsp))
 
 
-(use-package lsp-ui
+(use-package
+  lsp-ui
   :ensure t
   :commands lsp-ui-mode
-  :init
-  )
+  :init)
 
-(setq lsp-ui-doc-enable t
-      lsp-ui-peek-enable t
-      lsp-ui-sideline-enable t
-      lsp-ui-imenu-enable t
-      lsp-ui-flycheck-enable t
-      lsp-ui-sideline-toggle-symbols-info t)
+(setq lsp-ui-doc-enable t lsp-ui-peek-enable t lsp-ui-sideline-enable t lsp-ui-imenu-enable t
+      lsp-ui-flycheck-enable t lsp-ui-sideline-toggle-symbols-info t)
 
 ;; yasnippet
-(use-package yasnippet
+(use-package
+  yasnippet
   :ensure t
-  :init
-  (yas-global-mode 1)
-  :config
-  (use-package yasnippet-snippets :ensure t )
+  :init (yas-global-mode 1)
+  :config (use-package
+	    yasnippet-snippets
+	    :ensure t)
   (yas-reload-all))
 
 ;; terminal
-(setq-default
- shell-file-name "/bin/zsh")
+(setq-default shell-file-name "/bin/zsh")
 
 
 ;; lsp tuning
@@ -175,9 +175,32 @@
 (add-to-list 'lsp-file-watch-ignored "[/\\\\]data")
 
 
+;; org capture
+(setq org-capture-templates '(("t" "Todo [Inbox]" entry (file+headline "~/orgs/gtd.org" "Tasks")
+			       "* TODO %?\n  %i\n ")
+			      ("c" "Code" entry (file+headline "~/orgs/code.org" "Code")
+			       "* TODO %?\n %i\n %a")
+			      ("j" "Journal" entry (file+datetree "~/orgs/journal.org")
+			       "* %?\nEntered on %U\n  %i\n  %a")
+			      ("T" "Tickler" entry (file+headline "~/gtd/tickler.org" "Tickler")
+			       "* %i%? \n %U")))
 
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/orgs/gtdWork.org" "Tasks")
-	 "* TODO %?\n  %i\n  %a")
-	("j" "Journal" entry (file+datetree "~/org/journal.org")
-	 "* %?\nEntered on %U\n  %i\n  %a")))
+(setq org-refile-targets (quote (("~/orgs/tickler.org" :maxlevel . 3)
+				 ("~/orgs/gtd.org" :level . 2)
+				 ("~/orgs/someday.org" :level . 1))))
+
+(define-key global-map (kbd "C-c o")
+  (lambda ()
+    (interactive)
+    (org-capture)))
+(define-key global-map (kbd "C-c a")
+  (lambda ()
+    (interactive)
+    (org-agenda)))
+
+
+
+;; magit key  binding
+(global-set-key (kbd "C-c m") 'magit)
+;; org agenda
+(setq org-agenda-files (list "~/orgs/gtd.org" "~/orgs/code.org" "~/orgs/journal.org"))
