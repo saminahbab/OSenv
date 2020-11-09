d2383e3a57c329d7315b113ac41e382514e099d9
saminahbab <saminahbab0@gmail.com>
2020-11-09 16:47:42 +0000

    new symlinks

--- emacs/symlinks.sh
+++ emacs/symlinks.sh
@@ -5,7 +5,4 @@
 mkdir ~/.emacs.d/lisp
 
 ln -s $(pwd)/init.el ~/.emacs.d/init.el
-
-ln -s $(pwd)/general.el ~/.emacs.d/lisp/general.el
-ln -s $(pwd)/packages.el ~/.emacs.d/lisp/packages.el
-ln -s $(pwd)/go.el ~/.emacs.d/lisp/go.el
+ln -s $(pwd)/init.org ~/.emacs.d/lisp/init.org
