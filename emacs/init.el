7e9751953f26e266fd8f318961e274ace0ad169f
saminahbab <saminahbab0@gmail.com>
2020-11-14 19:03:36 +0000

    autogen packages

--- emacs/init.el
+++ emacs/init.el
@@ -10,7 +10,9 @@
  '(custom-safe-themes
    '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "dde8c620311ea241c0b490af8e6f570fdd3b941d7bc209e55cd87884eb733b0e" default))
  '(org-agenda-files
-   '("~/orgs/roam/20201112204422-database_issues.org" "~/orgs/gtd.org" "~/orgs/code.org" "~/orgs/journal.org")))
+   '("~/orgs/roam/20201112204422-database_issues.org" "~/orgs/gtd.org" "~/orgs/code.org" "~/orgs/journal.org"))
+ '(package-selected-packages
+   '(highlight-indent-guides yasnippet-snippets xterm-color use-package treemacs-projectile transpose-frame toml-mode tide terraform-mode spacemacs-theme spaceline smex smartparens smart-shift rjsx-mode ripgrep rainbow-mode rainbow-delimiters pyvenv python-black prettier-js org-roam-server org-roam-bibtex org-ref org-noter-pdftools org-journal org-download org-bullets ob-cypher markdown-preview-mode magit lsp-ui lsp-pyright lsp-docker k8s-mode jq-mode jetbrains-darcula-theme ivy-rich indium indent-tools highlight-indentation go-mode flycheck-yamllint flycheck-rust elisp-format ein doom-themes dockerfile-mode docker-compose-mode deft deadgrep dashboard cython-mode counsel-projectile company-lsp cargo buffer-move aggressive-indent aggressive-fill-paragraph)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
