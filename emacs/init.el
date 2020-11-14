942764e04ed52711f865f11c031d6387d8aa4f93
saminahbab <saminahbab0@gmail.com>
2020-11-14 15:57:18 +0000

    load configuration el

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,19 @@
 (add-to-list 'load-path "~/.emacs.d/lisp")
-(require 'org)
+(load "configuration")
 
 
-(org-babel-load-file (expand-file-name (concat user-emacs-directory "lisp/init.org")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "dde8c620311ea241c0b490af8e6f570fdd3b941d7bc209e55cd87884eb733b0e" default))
+ '(org-agenda-files
+   '("~/orgs/roam/20201112204422-database_issues.org" "~/orgs/gtd.org" "~/orgs/code.org" "~/orgs/journal.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
